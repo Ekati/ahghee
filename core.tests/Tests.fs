5e81ed9f3f872f38a849ecc969ba4f861a588ed3
Astn <austin.w.harris@gmail.com>
2018-04-27 23:28:02 -0600

    Added Graph, and basic managment functions

--- core.tests/Tests.fs
+++ core.tests/Tests.fs
@@ -6,13 +6,13 @@ open Ahghee
 open System
 
 
-let TestCreateInternalIRI = { NodeIRI.Domain = "biggraph://example.com"; NodeIRI.Database="test"; NodeIRI.Graph="People"; NodeIRI.NodeId="1"; NodeIRI.RouteKey= None} 
+let Id id = { NodeIRI.Domain = "biggraph://example.com"; NodeIRI.Database="test"; NodeIRI.Graph="People"; NodeIRI.NodeId=id; NodeIRI.RouteKey= None} 
 let TestCreateExternalIRI = ExternalIRI (System.Uri( "https://ahghee.com" )) 
 let TestCreateBinary = Binary { MimeBytes.Mime= Some("application/json"); MimeBytes.Bytes = Array.Empty<byte>() } 
 
 [<Fact>]
 let ``Can create an InternalIRI type`` () =
-    let d : Data = InternalIRI TestCreateInternalIRI 
+    let d : Data = InternalIRI (Id "1") 
     let success = match d with 
         | InternalIRI (nodeIRI) -> true
         | ExternalIRI (external) -> false
@@ -39,18 +39,18 @@ let ``Can create a Binary type`` () =
     Assert.True success   
 
 let mimePlainTextUtf8 = Some("text/plain;charset=utf-8")
-let BinaryText (text:string) = Binary { Mime = mimePlainTextUtf8 ; Bytes = Text.UTF8Encoding.UTF8.GetBytes(text) }
+let BStr (text:string) = Binary { Mime = mimePlainTextUtf8 ; Bytes = Text.UTF8Encoding.UTF8.GetBytes(text) }
     
+let Prop (key:Data) (values:seq<Data>) =
+    let pair = { Pair.Key = key; Value = (values |> Seq.toArray)}   
+    pair  
     
-let TestCreateStringPairs key (values:seq<string>) = 
-    let key = BinaryText key
-    let v = values |> Seq.map (fun item -> BinaryText item) |> Seq.toArray
-    let pair = { Pair.Key = key; Value = v}   
-    pair     
+let PropStr (key:string) (values:seq<string>) = Prop (BStr key) (values |> Seq.map(fun x -> BStr x))  
+let PropStrData (key:string) (values:seq<Data>) = Prop (BStr key) values      
     
 [<Fact>]
 let ``Can create a Pair`` () =
-    let pair = TestCreateStringPairs "firstName" [|"Richard"; "Dick"|]
+    let pair = PropStr "firstName" [|"Richard"; "Dick"|]
 
     let success = match pair.Key with 
                 | Binary (b) when b.Mime.IsSome && b.Mime.Value = mimePlainTextUtf8.Value -> true 
@@ -59,11 +59,57 @@ let ``Can create a Pair`` () =
     
 [<Fact>]
 let ``Can create a Node`` () =
+    let node = { 
+                Node.NodeIds = [| Id "1" |] 
+                Node.Attributes = [|
+                                    PropStr "firstName" [|"Richard"; "Dick"|] 
+                                  |]
+               }
 
-    let pair = TestCreateStringPairs "firstName" [|"Richard"; "Dick"|] 
+    let empty = node.Attributes |> Seq.isEmpty
+    Assert.True (not empty)
+    
+[<Fact>]
+let ``Can traverse local graph index`` () =
+    let node1 = { 
+                Node.NodeIds = [| Id "1" |] 
+                Node.Attributes = [|
+                                    PropStr "firstName" [|"Richard"; "Dick"|] 
+                                    PropStrData "follows" [| InternalIRI (Id "2") |] 
+                                  |]
+               }
+               
+    let node2 = { 
+                Node.NodeIds = [| Id "2" |] 
+                Node.Attributes = [|
+                                    PropStr "firstName" [|"Sam"; "Sammy"|] 
+                                    PropStrData "follows" [| InternalIRI (Id "1") |]
+                                  |]
+               }
+               
+    let node3 = { 
+                Node.NodeIds = [| Id "3" |] 
+                Node.Attributes = [|
+                                    PropStr "firstName" [|"Jim"|]
+                                    PropStrData "follows" [| InternalIRI (Id "1"); InternalIRI (Id "2") |] 
+                                  |]
+               }      
+               
+    let graph:Graph = new Graph()
+    graph.Add [| node1; node2; node3 |]
+    
+    let nodesWithIncomingEdges = node3.Attributes 
+                                         |> Seq.collect (fun y -> y.Value 
+                                                               |> Seq.map (fun x -> match x with  
+                                                               | InternalIRI(id) -> Some(id) 
+                                                               | _ -> None))   
+                                         |> Seq.filter (fun x -> match x with 
+                                                                 | Some id -> true 
+                                                                 | _ -> false)
+                                         |> Seq.map    (fun x -> x.Value )
+                                         |> Seq.distinct
+                                         |> Seq.map (fun x -> graph.TryFind x)                                                                            
 
-    let nodeId = TestCreateInternalIRI
-    let node = { Node.NodeIds = [|nodeId|]; Node.Attributes = [|pair|]}
+    Assert.NotEmpty nodesWithIncomingEdges
 
-    let hasSome = node.Attributes.Length > 0
-    Assert.True hasSome
+    
--- core/Types.fs
+++ core/Types.fs
@@ -2,13 +2,23 @@ namespace Ahghee
 
 open System
 
-type NodeIRI = { Domain: string; Database: string; Graph: string; NodeId: string; RouteKey: Option<string> }
-type MimeBytes = { Mime: Option<string>; Bytes : Byte[] }
 
+type MimeBytes = { Mime: Option<string>; Bytes : Byte[] } 
+type NodeIRI = { Domain: string; Database: string; Graph: string; NodeId: string; RouteKey: Option<string>} 
 type Data =
   | InternalIRI of NodeIRI
   | ExternalIRI of System.Uri
   | Binary of MimeBytes
-
-type Pair = { Key: Data; Value : Data[] }
-type Node = { NodeIds: NodeIRI[]; Attributes: Pair[] }
+type Pair = { Key: Data; Value : seq<Data> }
+type Node = { NodeIds: seq<NodeIRI>; Attributes: seq<Pair> }
+ 
+type Graph() =  
+    let mutable _nodes:seq<Node> = Seq.empty 
+    member x.Nodes = _nodes
+    member x.Add (nodes:seq<Node>) = _nodes <- Seq.append _nodes nodes
+    member x.TryFind (nodeIri:NodeIRI):Option<Node> = 
+        let isLocal = _nodes 
+                      |> Seq.tryFind ( fun n -> n.NodeIds |> Seq.exists (fun nn -> nn = nodeIri))
+        match isLocal with 
+        | Some node -> Some(node)
+        | None -> failwith "remote nodes not supported yet"                        
