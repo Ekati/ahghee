0dcf7b8500efd4c941a5ae9edad6233b94e80ae2
Astn <austin.w.harris@gmail.com>
2018-04-28 21:20:38 -0600

    Added test for int type attribute from tinkerpop-crew.xml

--- core.tests/Tests.fs
+++ core.tests/Tests.fs
@@ -5,12 +5,16 @@ open Xunit
 open Xunit.Abstractions
 open Ahghee
 open System
+open System.Collections
 open System.IO
 open System.Text
 open System.Threading.Tasks
 open FSharp.Data
 
-let mimePlainTextUtf8 = Some("text/plain;charset=utf-8")
+let mimePlainTextUtf8 = Some("xs:string")
+let mimeXmlInt = Some("xs:int")
+let mimeXmlDouble = Some("xs:double")
+
 let BStr (text:string) = BinaryBlock (MimeBytes { Mime = mimePlainTextUtf8 ; Bytes = Text.UTF8Encoding.UTF8.GetBytes(text) })
     
 let Prop (key:Data) (values:seq<Data>) =
@@ -102,7 +106,25 @@ type MyTests(output:ITestOutputHelper) =
         [| node1; node2; node3 |]      
         |> Array.toSeq             
     
+    member __.xsType graphMlType : Option<string> =
+                match graphMlType with
+                | "string" -> mimePlainTextUtf8
+                | "int" -> mimeXmlInt
+                | "double" -> mimeXmlDouble
+                | _ -> None
+    
     member __.buildNodesTheCrew : seq<Node> =
+        let attrs forType= 
+            TheCrew.Keys 
+            |> Seq.ofArray
+            |> Seq.filter (fun k -> k.For = forType)
+            |> Seq.map (fun k -> k.Id, (k.AttrName, k.AttrType))
+            |> Map.ofSeq
+        let NodeAttrs = attrs "node" 
+        let EdgeAttrs = attrs "edge" 
+        
+        
+                                
         TheCrew.Graph.Nodes 
         |> Seq.map (fun n -> 
                              { 
@@ -110,14 +132,35 @@ type MyTests(output:ITestOutputHelper) =
                                 Node.Attributes = n.Datas
                                                   |> Seq.ofArray
                                                   |> Seq.map (fun d ->
-                                                                { KeyValue.Key= BinaryBlock (MimeBytes {
-                                                                                                        MimeBytes.Mime=mimePlainTextUtf8;
-                                                                                                        Bytes=Encoding.UTF8.GetBytes d.Key })
-                                                                  Value= [BinaryBlock (MimeBytes {
-                                                                                                    MimeBytes.Mime=mimePlainTextUtf8;
-                                                                                                    Bytes= match d.String with 
-                                                                                                           | Some(s) -> Encoding.UTF8.GetBytes s
-                                                                                                           | _ -> Array.empty<byte>})]
+                                                                let keyBytes = 
+                                                                    let (name, typ) = NodeAttrs.Item d.Key
+                                                                    Encoding.UTF8.GetBytes name
+                                                                
+                                                                let valueMime =
+                                                                    let (name, typ) = NodeAttrs.Item d.Key 
+                                                                    __.xsType typ
+                                                                
+                                                                let valueBytes =
+                                                                    match valueMime with
+                                                                    | m when m = mimePlainTextUtf8 -> match d.String with  
+                                                                                           | Some(s) -> Encoding.UTF8.GetBytes s
+                                                                                           | _ -> Array.empty<byte>
+                                                                    | m when m = mimeXmlDouble -> match d.String with  
+                                                                                       | Some(s) -> BitConverter.GetBytes (double s)
+                                                                                       | _ -> Array.empty<byte>
+                                                                    | m when m = mimeXmlInt -> match d.String with  
+                                                                                    | Some(s) -> BitConverter.GetBytes (int32 s)
+                                                                                    | _ -> Array.empty<byte>
+                                                                    | _ -> Array.empty<byte>                                                                                    
+                                                                
+                                                                { 
+                                                                    KeyValue.Key= BinaryBlock (MimeBytes {
+                                                                                                        MimeBytes.Mime= mimePlainTextUtf8;
+                                                                                                        Bytes= keyBytes
+                                                                                                             })
+                                                                    Value= [BinaryBlock (MimeBytes {
+                                                                                                    MimeBytes.Mime= valueMime
+                                                                                                    Bytes= valueBytes})]
                                                                 }
                                                              )    
                              }
@@ -247,7 +290,7 @@ type MyTests(output:ITestOutputHelper) =
                                                                 let values = attr.Value
                                                                             |> Seq.map (fun v -> match v with
                                                                                                  | BinaryBlock(MimeBytes mb) when mb.Mime = mimePlainTextUtf8 ->
-                                                                                                    Encoding.UTF8.GetString mb.Bytes
+                                                                                                      Encoding.UTF8.GetString mb.Bytes
                                                                                                  | _ -> "" 
                                                                                        )                                                                                                           
                                                                 values 
@@ -268,6 +311,63 @@ type MyTests(output:ITestOutputHelper) =
                            ]
                            
          Assert.Equal<string * string * string>(expected,actual)
+
+    member __.CollectValues key (graph:Graph) =
+        graph.Nodes
+             |> Seq.collect (fun n -> n.Attributes 
+                                      |> Seq.filter (fun attr -> match attr.Key with 
+                                                                 | BinaryBlock(MimeBytes mb) when mb.Mime = mimePlainTextUtf8 -> 
+                                                                   ( key , Encoding.UTF8.GetString mb.Bytes) |> String.Equals 
+                                                                 | _ -> false
+                                                    )
+                                      |> Seq.map    (fun attr -> n, attr) 
+                                      |> Seq.collect (fun (n,attr) -> 
+                                                    let _id = n.NodeIDs |> Seq.head |> (fun id -> match id with    
+                                                                                  | NodeID(nid) -> nid.NodeId
+                                                                                  | MemoryPointer(mp) -> "")  
+                                                                                  
+                                                    let labelV = match attr.Key with 
+                                                                 | BinaryBlock(MimeBytes mb) when mb.Mime = mimePlainTextUtf8 ->
+                                                                        Encoding.UTF8.GetString mb.Bytes
+                                                                 | _ -> ""
+                                                                 
+                                                    let values = attr.Value
+                                                                |> Seq.map (fun v -> match v with
+                                                                                     | BinaryBlock(MimeBytes mb) ->
+                                                                                          mb.Mime, mb.Bytes
+                                                                                     | _ -> None,Array.empty<byte> 
+                                                                           )                                                                                                           
+                                                    values 
+                                                    |> Seq.map (fun (at,v) -> _id,labelV,at,v)
+                                                 )                                                           
+                             )
+             |> List.ofSeq
+         
+    [<Fact>] 
+    member __.``After load tinkerpop-crew.xml Age has mime type int and comes out as int`` () =
+         let g:Graph = new Graph(new MemoryStore())
+         let nodes = __.buildNodesTheCrew
+         let task = g.Add nodes
+         match task.Status with
+             | TaskStatus.Created -> task.Start()
+             | _ -> ()                                                                     
+         task.Wait()
+                  
+         output.WriteLine("g.Nodes length: {0}", g.Nodes |> Seq.length )
+         let attrName = "age"
+         let actual = __.CollectValues attrName g                                         
+              
+         
+                                       
+         let expected = [ 
+                                "1",attrName,mimeXmlInt, BitConverter.GetBytes 29;
+                                "2",attrName,mimeXmlInt, BitConverter.GetBytes 27;
+                                "4",attrName,mimeXmlInt, BitConverter.GetBytes 32;
+                                "6",attrName,mimeXmlInt, BitConverter.GetBytes 35; 
+                           ]
+         output.WriteLine(sprintf "foundData: %A" actual)
+         output.WriteLine(sprintf "expectedData: %A" expected)
+         Assert.Equal<string * string * Option<string> * byte[]>(expected,actual)         
 
 //    [<Fact>]
 //    member __.``I can use the file api`` () =
